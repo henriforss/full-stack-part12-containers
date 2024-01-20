2c7aec96541b1dbcda748564db6b1adfdfdc83b6
Henri Forss <hforss@gmail.com>
2024-01-20 13:16:07 +0200

    12.19 done

--- todo-app/todo-frontend/src/App.js
+++ todo-app/todo-frontend/src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
-import TodoView from './Todos/TodoView'
+import "./App.css";
+import TodoView from "./Todos/TodoView";
 
 function App() {
   return (
     <div className="App">
+      <h1>Welcome!</h1>
       <TodoView />
     </div>
   );
